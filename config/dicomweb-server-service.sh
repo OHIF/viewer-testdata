b7a937ee8be043844a642d31787ee26d61039d8d
Bill Wallace <wayfarer3130@gmail.com>
2022-01-27 12:51:47 -0500

    Creating a dicomweb branch of viewer-testdata
    This branch will be
    periodically updates with a version of the testdata generated using
    static-wado from the DICOM data.

--- config/dicomweb-server-service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /usr/src/app
-npm start
--- config/server-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: 'development',
-    dbServer: 'http://0.0.0.0',
-    db: 'chronicle',
-    dbPort: process.env.PORT || 5984,
-    auth: 'none',
-    logger: true,
-  };
